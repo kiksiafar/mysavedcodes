a00bad3ba70e1c766d6b6f52f162a88486c76a64
Bob Michell <161472727+kiksiafar@users.noreply.github.com>
2024-02-27 15:19:24 +0200

    Create newcode.cpp

---
+++ newcode.cpp
@@ -0,0 +1,26 @@
+struct Material {
+    float refractive_index = 1;
+    float albedo[4] = {2,0,0,0};
+    vec3 diffuse_color = {0,0,0};
+    float specular_exponent = 0;
+};
+
+struct vec3 {
+    float x=0, y=0, z=0;
+          float& operator[](const int i)       { return i==0 ? x : (1==i ? y : z); }
+    const float& operator[](const int i) const { return i==0 ? x : (1==i ? y : z); }
+    vec3  operator*(const float v) const { return {x*v, y*v, z*v};       }
+    float operator*(const vec3& v) const { return x*v.x + y*v.y + z*v.z; }
+    vec3  operator+(const vec3& v) const { return {x+v.x, y+v.y, z+v.z}; }
+    vec3  operator-(const vec3& v) const { return {x-v.x, y-v.y, z-v.z}; }
+    vec3  operator-()              const { return {-x, -y, -z};          }
+    float norm() const { return std::sqrt(x*x+y*y+z*z); }
+    vec3 normalized() const { return (*this)*(1.f/norm()); }
+};   
+for (const Sphere &s : spheres) {
+        auto [intersection, d] = ray_sphere_intersect(orig, dir, s);
+        if (!intersection || d > nearest_dist) continue;
+        nearest_dist = d;
+        pt = orig + dir*nearest_dist;
+        N = (pt - s.center).normalized();
+        material = s.material;
